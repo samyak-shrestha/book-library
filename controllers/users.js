e55debb3210bc4e39936957008c472bf6ef8f3cb
samyak shrestha <sams.stha@gmail.com>
2020-04-25 21:11:22 +0545

    added jwt and token and passwork encryption

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,10 @@ router.post('/', async(req, res)=>{
 
     if(error) return res.status(400).send(error.details[0].message);
 
-    let user = new User(_.pick(req.body, ['firstName','middleName','lastName', 'email', 'password']));
+    let user = await User.findOne({email: req.body.email});
+    if(user) return res.status(400).send('User already registered');
+
+    user = new User(_.pick(req.body, ['firstName','middleName','lastName', 'email', 'isAdmin','password']));
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
